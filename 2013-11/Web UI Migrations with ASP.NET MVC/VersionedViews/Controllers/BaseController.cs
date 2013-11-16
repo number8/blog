6efb858f8afdbef33d349e6fc7bfa584cc40e912
Mauricio Morales <mauricio.morales.zumbado@gmail.com>
2013-11-15 19:01:34 -0600

    Initial Empty ASP.NET MVC Site

---
+++ 2013-11/Web UI Migrations with ASP.NET MVC/VersionedViews/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VersionedViews.Controllers
+{
+    /// <summary>
+    /// Our Base Controller Class that we'll use as base of all of our Controllers.
+    /// We'll define common behavior here.
+    /// </summary>
+    public abstract class BaseController : Controller
+    {
+        public virtual int UIVersion
+        {
+            get { return 1; }
+        }
+    }
+}
---
+++ 2013-11/Web UI Migrations with ASP.NET MVC/VersionedViews/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VersionedViews.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public override int UIVersion
+        {
+            get { return base.UIVersion; }
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
